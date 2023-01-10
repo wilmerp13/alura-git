37fbf5241619ded1a80c3627772557fd3939f9e9
Wilmer <wilmerpolanco13@gmail.com>
2023-01-10 11:14:52 -0500

    created encrypter and decrypter logic and printing to console log

--- Alura HTML/encriptador/script.js
+++ Alura HTML/encriptador/script.js
@@ -1,12 +1,61 @@
+let texto = document.querySelector('textarea');
+texto.focus();
 
-
-
-const encriptador = function(){
-
+document.querySelector('.encriptar').addEventListener('click', function(){
+    console.log((texto).value);
+    // La letra "e" es convertida para "enter"
+    // La letra "i" es convertida para "imes"
+    // La letra "a" es convertida para "ai"
+    // La letra "o" es convertida para "ober"
+    // La letra "u" es convertida para "ufat"
+  if (texto.value === 'e'){
+    texto.value = 'enter';
+}
+  else if (texto.value === 'i'){
+           texto.value = 'imes';
 
 }
-
-const desencriptador = function (){
-
-
+  else if (texto.value === 'a'){
+           texto.value = 'ai';
 }
+
+else if (texto.value === 'o'){
+         texto.value = 'ober';
+}
+
+else if (texto.value === 'u'){
+         texto.value = 'ufat';
+}
+
+
+console.log(texto.value);
+   
+   
+
+});
+
+
+
+document.querySelector('.desencriptar').addEventListener('click', function(){
+
+    if (texto.value === 'enter'){
+        texto.value = 'e';
+    }
+      else if (texto.value == 'imes'){
+               texto.value = 'i';
+    
+    }
+      else if (texto.value == 'ai'){
+               texto.value = 'a';
+    }
+    
+    else if (texto.value == 'ober'){
+             texto.value = 'o';
+    }
+    
+    else if (texto.value == 'ufat'){
+             texto.value = 'u';
+    }
+    console.log(texto.value);
+
+});
