30040b058ba2d19407f1be3d819ed10afb3ceb09
Wilmer <wilmerpolanco13@gmail.com>
2023-02-10 17:47:02 -0500

    practicing java Alura 1st module

---
+++ ejemplo.java
@@ -0,0 +1,24 @@
+
+public class Ejemplo {
+
+public static void main(String[] args) {
+	
+System.out.println("Hello World");	
+
+int edad = 19;
+int persona = 1;
+
+if (edad > 18) {
+System.out.println("Estas autorizado para entrar ya que tu edad es "+ edad + "!");
+}
+else if(persona > 1){ 
+	System.out.println("Puedes entrar ya que andas acompañado");
+}
+	
+
+else {
+	System.out.println("Lamentamos que no puedes entrar, tu eres menor de edad");
+	}
+}
+
+}
