6f893ba18b0a70dabeaae86a2e90a09707bb928a
Wilmer <wilmerpolanco13@gmail.com>
2023-02-13 21:13:42 -0600

    added deposit and withdrawal methods

--- bytebank/projecto-bytebank/src/Cuenta.java
+++ bytebank/projecto-bytebank/src/Cuenta.java
@@ -4,4 +4,22 @@ class Cuenta {
 	int agencia;
 	int	numero;
 	String titular;
+	
+	
+	// No retorna valor
+	public void depositar(double valor) { 
+		this.saldo = this.saldo + valor;
+		
+	}
+	
+	//retorna valor
+	public boolean retirar(double valor){
+		if (this.saldo >= valor) {
+			this.saldo = this.saldo - valor;
+			return true;
+		}
+	}
+	
+	
+	
 }
